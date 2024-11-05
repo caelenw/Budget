fd827ba8cbc0e5e607750f237a225f12ad589955
caelen wall <cswall@email.sc.edu>
2024-11-05 17:59:21 -0500

    update

--- react-layout/src/Layout.js
+++ react-layout/src/Layout.js
@@ -1,4 +1,6 @@
+import React from 'react';
 import { Outlet } from "react-router-dom";
+
 import Footer from "./components/Footer";
 import Header from "./components/Header"; // Ensure this path is correct now
 const Layout = () => {
